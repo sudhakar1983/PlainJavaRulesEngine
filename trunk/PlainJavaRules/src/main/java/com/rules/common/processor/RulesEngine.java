7c37ecc23051e2ab6888fa7894c01de07b5bde22
sudhakar.duraiswamy@gmail.com <sudhakar.duraiswamy@gmail.com@2af97c62-1f7d-4477-0a94-19af9260bf06>
2012-10-31 14:37:30 +0000

    scrapped

--- trunk/PlainJavaRules/src/main/java/com/rules/common/processor/RulesEngine.java
+++
@@ -1,9 +0,0 @@
-package com.rules.common.processor;
-
-import com.rules.common.TechnicalException;
-
-
-public interface RulesEngine {
-	
-	public boolean processRule(Object object , int ruleId) throws TechnicalException ;
-}
