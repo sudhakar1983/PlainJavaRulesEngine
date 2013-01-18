35cb9a0b0e2b3f1afab71295ebb470efaafb742e
sudhakar.duraiswamy@gmail.com <sudhakar.duraiswamy@gmail.com@2af97c62-1f7d-4477-0a94-19af9260bf06>
2013-01-18 13:52:34 +0000

    Initial Draft

---
+++ trunk/pjr-client/src/main/java/org/pjr/rulesengine/ui/RulesEngineImpl.java
@@ -0,0 +1,17 @@
+package org.pjr.rulesengine.ui;
+
+public class RulesEngineImpl implements RulesEngine{
+
+	@Override
+	public Object process(ExecutionMode executionMode, String fullyQualifiedClassName, Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object processSingleRule(String fullyQualifiedClassName, Object object, String ruleId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
