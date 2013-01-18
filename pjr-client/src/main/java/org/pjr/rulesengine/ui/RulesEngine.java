3a7b90c39341dae99342cec43e772d1733c630f7
sudhakar.duraiswamy@gmail.com <sudhakarbe@gmail.com>
2013-01-18 13:52:34 +0000

    Initial Draft

---
+++ pjr-client/src/main/java/org/pjr/rulesengine/ui/RulesEngine.java
@@ -0,0 +1,17 @@
+package org.pjr.rulesengine.ui;
+
+public interface RulesEngine {
+
+	enum ExecutionMode{
+		ELSEIF_MODE,
+		EVAULATE_ALL_MODE,
+		SINGLEDECISIONMODE
+	}
+	
+	
+	public Object process(final ExecutionMode executionMode ,final  String fullyQualifiedClassName, final Object object);
+	
+	public Object processSingleRule(final  String fullyQualifiedClassName, final Object object , final String ruleId);
+	
+	
+}
